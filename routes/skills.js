bcca357f175d6e9ec4efe9f37bf3828430514c3a
Molly Calhoun <mollyfcalhoun@gmail.com>
2024-02-15 08:50:52 -0800

    Update show.ejs

---
+++ routes/skills.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const skillsCtrl = require("../controllers/skills");
+
+router.get("/", skillsCtrl.index);
+
+router.get("/:id", skillsCtrl.index);
+
+router.get("/new", skillsCtrl.new);
+
+router.get("/:id", skillsCtrl.show);
+
+module.exports = router;
+
+// GET /skills
